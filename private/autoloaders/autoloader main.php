2d800dca0386937cff2834fe4fc0908a7c88456f
Cody Swartz <ctsstc@gmail.com>
2014-12-04 23:56:13 -0800

    Updated with an autoloader that maps namespaces to folders in the models folder

--- private/autoloaders/autoloader main.php
+++ private/autoloaders/autoloader main.php
@@ -1,9 +1,21 @@
 <?
 /* Model Paths Load Only When Needed via spl_autoload */
-//define('MODEL_PAGE', '../private/models/Page.php');
 spl_autoload_register(function ($class) {
-	$path = '../private/models/' . $class . '.php';
-	if (file_exists($path))
+	$nameSpaced = strpos($class, '\\');
+	// If name spaced
+	if ($nameSpaced !== false) {
+		// remove first character \
+		$class = ltrim($class, '\\');;
+		// Switch back slashes to forward slashes / directory slashes
+		$class  = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+	}
+	$path = __DIR__.'/../models/' . $class . '.php';
+
+	// See what's being included
+	//echo "Including: >>$path<< <br>";
+	//stackTrace();
+
+	if (is_file($path) && is_readable($path))
 		include $path;
 });
 ?>
